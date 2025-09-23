a46aa2b44d94978ebfcda9cc554b55943bcd50d6
José <josevictorengcomp@gmail.com>
2025-09-23 12:07:27 -0300

    feat: definindo parametros para a dag

--- dags/dag_ingest_techstyle_data.py
+++ dags/dag_ingest_techstyle_data.py
@@ -1,9 +1,19 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow.sdk import DAG, task
 from airflow.providers.standard.operators.bash import BashOperator
 from airflow.providers.postgres import PostgresOperator
 
 dag = DAG(
-    dag_id = 'Ingest_dag'
-    
+    dag_id = 'Ingest_dag',
+    schedule = '@daily',
+    start_date = datetime(2025, 9, 23),
+    description = 'Dag para realizar a ingestão de dados dos arquivos csv',
+    default_args = {
+        'depends_on_past': False,
+        'retries': 3,
+        'retry_delay': timedelta(minutes=5)
+    },
+    catchup = False,
+    tag = ['ingestao']
 )
+t1 = BashOperator
