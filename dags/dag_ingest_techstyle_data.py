7abe0ae8bebbf3c48401cd214f8a09ca1a8ce971
José <josevictorengcomp@gmail.com>
2025-09-25 17:37:44 -0300

    fix: erro de funcionamento da task corrigido

--- dags/dag_ingest_techstyle_data.py
+++ dags/dag_ingest_techstyle_data.py
@@ -1,15 +1,28 @@
 from datetime import datetime, timedelta
-from airflow.decorators import dag, task
-from airflow.providers.standard.operators.bash import BashOperator
-from airflow.models.variable import Variable
+from airflow.decorators import dag
+from airflow.providers.standard.operators.python import PythonOperator
 from plugins.scripts.ingest_data import ingestao_data
+from airflow.models.variable import Variable
 import os
         
 '''
-1- Fazer a função para listar os arquivos CSV
-2- Realizar a validação via linha de comando
-3- Se a validação passar, processa o arquivo com o plugin de ingestão
+Como existe uma ordem específica de ingestão a ser seguida a melhor opção é criar um estrutura estática
 '''
+# Trazendo o caminho configurado dentro de uma variável
+# O erro Erro 0x80004005 mesmo sendo um erro genérico ele também pode estar associado a problemas
+# de acesso a pastas ou unidades compartilhadas
+CAMINHO_BASE = Variable.get('caminho_pasta', default_var='Erro 0x80004005') 
+
+def chamar_arquivo_ingestao(nome_arquivo, nome_tabela):
+    caminho_config = os.path.join(CAMINHO_BASE, nome_arquivo)
+    if not os.path.exists(caminho_config):
+        raise FileNotFoundError(f'Arquivo não encontrado: {caminho_config}')
+    
+    with open(caminho_config, 'r') as f:
+        if len(f.readlines()) <= 1:
+            raise ValueError(f'Arquivo {caminho_config} está vazio ou contém apenas um cabeçalho')
+    
+    ingestao_data(caminho_do_arquivo=caminho_config, nome_tabela_destino=nome_tabela)
 
 @dag(
     dag_id = 'Ingest_dynamic_dag',
@@ -25,85 +38,27 @@ import os
     catchup = False,
     tags = ['ingestao', 'dynamic']
 ) 
-    
-def pipeline_dinamica():
-    # Configurando uma função para listar todos os arquivos CSV de uma pasta configurada via variável
-    @task(task_id='Arquivos_CSV')
-    def listar_arquivos_csv() -> list[str]:
-        caminho_config = Variable.get('caminho_pasta')
-        print(f'buscando arquivos na pasta confirguirada: "{caminho_config}" .')
-        arquivos_csv = [
-            os.path.join(caminho_config, f)
-            for f in os.listdir(caminho_config)
-            if f.endswith('.csv')
-        ]
-        if not arquivos_csv:
-            raise ValueError(f'Nenhum arquivo CSV encontrado: {caminho_config}')
-        return arquivos_csv
 
-    """
-    Caminho_config -> Variável que receber o caminho configurado
-    Variable.get -> Pode ser alterado sem reiniciar o Airflow. Utilizada para valores que precisam que sejam
-    dinâmicos e facilmente ajustados pela UI do Airflow sem um novo deploy.
-    """
+def pipeline_em_ordem():
     
-        
-    validar_arquivo_bash = BashOperator.partial(
-        task_id = 'validar_arquivo',
-        params = {'caminho_do_arquivo': ''},
-        bash_command = """
-        FILEPATH ="{{ caminho_do_arquivo }}" 
-        echo "Validando o arquivo: "$FILEPATH" "
-        LINE_COUNT=$(wc -l < "$FILEPATH")
-        if [ "$LINE_COUNT" -le 1 ]; then
-            echo "O caminho do arquivo "$FILEPATH" está vazio ou contem apenas o cabeçalho"
-        else
-            echo "O caminho do arquivo "$FILEPATH" contém "$LINE_COUNT" linhas e aparenta ser válido"
-        fi
-        """   
+    ingest_clientes = PythonOperator(
+        task_id = 'ingestao_clientes',
+        python_callable = chamar_arquivo_ingestao,
+        op_kwargs = {'nome_arquivo': 'clientes.csv', 'nome_tabela': 'clientes_stagging'}
     )
-
-    """
-    FILEPATH -> Está sendo declarada a variável em bash.
-    echo -> Fazendo uma analogia com print(f'') em python.
-    $FILEPATH - chama a variável dentro do comando echo.
-    lINE_COUNT - defini uma variável para contar as linhas dos arquivos.
-    wc -> significa word count no unix e linux, é utilizado para contar mais do que palavras.
-    -l -> está especificando para o word count para contar as linhas dos arquivos.
-    "<" -> é um símbolo de redirecionamento de entrada; No comando acima, ele pega o conteúdo de FILEPATH e empurra
-    para o comando a esquerda.
-    -le -> Em bash essa linha não significa uma flag(opção) assim como "-l" mas sim ela é um operador de comparação 
-    "menor ou igual a" ou "Less than or Equal to"
-    []; -> onde fica a lógica do if
-    then -> caso seja verdadeiro o if pula para o bl
-    """
-
-    @task
-    def processar_ingest(caminho_do_arquivo: str) -> list[str]:
-        #Será montada uma lógica para chamar cada arquivo listado em arquivos_csv
     
-        tabela_alvo = "Dados_stagging"
-        resultado = ingestao_data(
-            caminho_do_arquivo =  caminho_do_arquivo,
-            nome_tabela_destino = tabela_alvo
-        )
-        print(resultado)
-                  
-    # 1. Obter a listar dos arquivos 
-    lista_de_arquivos = listar_arquivos_csv()
+    ingest_produtos = PythonOperator(
+        task_id = 'ingestao_produtos',
+        python_callable = chamar_arquivo_ingestao,
+        op_kwargs = {'nome_arquivo': 'produtos.csv', 'nome_tabela': 'produtos_stagging'}
+    )
     
-    # 2. Expandir a tarefa bash para cada arquivo da lista
-    tarefa_bash_mapeada = validar_arquivo_bash.expand(caminho_do_arquivo=lista_de_arquivos)
-        
-    # 3. Expandir a tarefa python para cada arquivo validado da lista
-    tarefa_python_mapeada = processar_ingest.expand(caminho_do_arquivo=lista_de_arquivos)
-        
-    """ 
-    4- Definindo a ordem de execução
-    - A tarefa de validação bash é executada após a listagem
-    - A tarefa de processamento é executado após a validação
-    """
-        
-    tarefa_bash_mapeada >> tarefa_python_mapeada
-                
-pipeline_dinamica()
+    ingest_pedidos = PythonOperator(
+        task_id = 'ingestao_pedidos',
+        python_callable = chamar_arquivo_ingestao,
+        op_kwargs = {'nome_arquivo': 'pedidos.csv', 'nome_tabela': 'pedidos_stagging'}
+    )
+    
+    [ingest_clientes, ingest_produtos] >> ingest_pedidos
+
+pipeline_em_ordem()
