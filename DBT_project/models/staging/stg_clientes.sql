b1cf2bc1e7ce783cb1f4a6af731f133730476f73
José <josevictorengcomp@gmail.com>
2026-03-18 19:02:17 -0300

    feat: rename columns

--- DBT_project/models/staging/stg_clientes.sql
+++ DBT_project/models/staging/stg_clientes.sql
@@ -1,1 +1,7 @@
 SELECT * FROM 'clientes';
+ALTER TABLE clientes RENAME COLUMN customer_id to 'CustomerID';
+ALTER TABLE clientes RENAME COLUMN customer_unique_id to 'UniqueID';
+ALTER TABLE clientes RENAME COLUMN customer_zip_code_prefix to 'ZipCodePrefix';
+ALTER TABLE clientes RENAME COLUMN customer_zip_code_prefix to 'ZipCodePrefix';
+ALTER TABLE clientes RENAME COLUMN customer_city to 'City';
+ALTER TABLE clientes RENAME COLUMN customer_state to 'State';
