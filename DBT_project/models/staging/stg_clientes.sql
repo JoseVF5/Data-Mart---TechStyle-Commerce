59eb65b1ff4954fad4c839f698309dabe7b7562d
José <josevictorengcomp@gmail.com>
2026-03-18 19:37:27 -0300

    feat: data casting

--- DBT_project/models/staging/stg_clientes.sql
+++ DBT_project/models/staging/stg_clientes.sql
@@ -1,7 +1,10 @@
-SELECT * FROM 'clientes';
-ALTER TABLE clientes RENAME COLUMN customer_id to 'CustomerID';
-ALTER TABLE clientes RENAME COLUMN customer_unique_id to 'UniqueID';
-ALTER TABLE clientes RENAME COLUMN customer_zip_code_prefix to 'ZipCodePrefix';
-ALTER TABLE clientes RENAME COLUMN customer_zip_code_prefix to 'ZipCodePrefix';
-ALTER TABLE clientes RENAME COLUMN customer_city to 'City';
-ALTER TABLE clientes RENAME COLUMN customer_state to 'State';
+SELECT * FROM "Clientes";
+ALTER TABLE "Clientes" RENAME COLUMN customer_id to "ID";
+ALTER TABLE "Clientes" RENAME COLUMN customer_unique_id to "UniqueID";
+ALTER TABLE "Clientes" RENAME COLUMN customer_zip_code_prefix to "ZipCodePrefix";
+ALTER TABLE "Clientes" RENAME COLUMN customer_city to "City";
+ALTER TABLE "Clientes" RENAME COLUMN customer_state to "State";
+UPDATE "Clientes";
+
+
+ALTER TABLE "Clientes" ALTER COLUMN "ZipCodePrefix" TYPE numeric(6);
